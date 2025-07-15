1c01752f13a70330fe0739ed10c80ff95c747b63
Muhammad Rizky Ardiansah <rizkyardiansah.dev@gmail.com>
2025-07-15 14:20:52 +0700

    Add CORS configuration to API routes and update dependencies

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"net/http"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/rizky-ardiansah/event-api/internal/env"
 	swaggerFiles "github.com/swaggo/files"
@@ -11,6 +13,15 @@ import (
 
 func (app *application) routes() http.Handler {
 	g := gin.Default()
+
+	config := cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}
+	g.Use(cors.New(config))
 
 	v1 := g.Group("/api/v1")
 	{
